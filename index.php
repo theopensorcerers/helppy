58573e743266e9a6896a404780c6934e012c7f90
Anais Moisy <anais.moisy@gmail.com>
2015-02-17 16:44:49 +0000

    add color images + stylise skill section in profile

--- index.php
+++ index.php
@@ -3,44 +3,38 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-         <div class="search-container">
-          <div class="space70">
-            <div class="search-box">
-              <div class="input-group search-container">
-                <input type="text" class="form-control" placeholder="Search for...">
-                <span class="input-group-btn">
-                  <button class="btn btn-default" type="button"><i class="fa fa-search fa-2x"></i></button>
-                </span>
-              </div>
-            </div>
+           <div class="search-container">
             <div class="space70">
-            </div>
-          </div> 
-        </div>
-        <div class="text-center">
-          <a class="logo"> 
-            <img src="images/logo.png" >
-          </a> 
-          <p>
-           
+              <div class="search-box">
+                <div class="input-group search-container">
+                  <input type="text" class="form-control" placeholder="Search for...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-default" type="button"><i class="fa fa-search fa-2x"></i></button>
+                  </span>
+                </div>
+              </div>
+              <div class="space70">
+              </div>
+            </div> 
+          </div>
+          <div class="text-center">
+            <a class="logo"> 
+              <img src="images/logo.png" >
+            </a> 
+            <p>
               <br>
               HELPPY is a free platform to collaborate, offer and receive skills
-                 
-                  <p> <strong> FIND </strong> the skills you are missing <br>
-                  <strong> GET </strong> the help needed <br>
-                  <strong> FOSTER </strong> collaboration <br>
-                  <strong> CREATE </strong> great projects </p>
-                 
-               
-            
-
-
-          </p>
-        </div>       
+              <p> <strong> FIND </strong> the skills you are missing <br>
+                    <strong> GET </strong> the help needed <br>
+                    <strong> FOSTER </strong> collaboration <br>
+                    <strong> CREATE </strong> great projects 
+              </p>
+            </p>
+          </div>       
        </div>
-      
     </div>
     
+
      <div class="container filter-tabs"> 
      	<h3>Filters</h3>
        <ul class="nav nav-tabs">
--- profile.php
+++ profile.php
@@ -14,7 +14,9 @@
 							    </div>
 					    </div>
 				</div>
-				<div class="col-xs-12 col-md-8">
+				<div class="col-xs-12 col-md-1">
+				</div>
+				<div class="col-xs-12 col-md-7">
 					<p>I've helped others</p>
 					<div class="progress">
 						<div class="progress-bar offered" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
@@ -38,6 +40,64 @@
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<div class="space20"></div>
+
+			<div class="row" >
+    			<div class="span12" style="border: 1px solid grey">
+    			<div class="span12" style="padding: 30px">
+					<p><strong>Skills</strong></p> <br>
+					<div class="row">
+				      <div class="col-xs-6 col-md-3">
+				        <a href="#" class="thumbnail">
+				          	<img alt="images/blue.png" src="images/blue.png" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;" > 
+  							<span class="thumbnail-label"><h3>Photoshop</h3></span>
+				        </a>
+				      </div>
+				      <div class="col-xs-6 col-md-3">
+				        <a href="#" class="thumbnail">
+							<img alt="images/green.png" src="images/green.png" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;">
+							<span class="thumbnail-label"><h3>Photography</h3></span>
+				        </a>
+				      </div>
+				      <div class="col-xs-6 col-md-3">
+				        <a href="#" class="thumbnail">
+							<img alt="images/blue.png" src="images/blue.png" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;">
+							<span class="thumbnail-label"><h3>InDesign</h3></span>
+				        </a>
+				      </div>
+				      <div class="col-xs-6 col-md-3">
+				        <a href="#" class="thumbnail">
+							<img alt="images/purple.png" src="images/purple.png" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;">
+							<span class="thumbnail-label"><h3>CSS</h3></span>
+				        </a>
+				      </div>
+				    </div>
+				    <div class="row">
+				      <div class="col-xs-6 col-md-3">
+				        <a href="#" class="thumbnail">
+							<img alt="images/orange.png" src="images/orange.png" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;">
+							<span class="thumbnail-label"><h3>Model making</h3></span>
+				        </a>
+				      </div>
+				      <div class="col-xs-6 col-md-3">
+				        <a href="#" class="thumbnail">
+							<img alt="images/deepblue.png" src="images/deepblue.png" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;">
+							<span class="thumbnail-label"><h3>Sewing</h3></span>
+				        </a>
+				      </div>
+				   	</div>
+				</div>
+			    </div>
+			</div>
+
+
+
+
+
+			
+		  
+		</div>  
 	</div>
-</div>	
+</div>
+	
