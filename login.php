fedac3fd9400c6a4a9093ff0eab90d47432ad3be
Mariela Barzallo <marielabarzallo@gmail.com>
2015-02-18 22:17:09 +0000

    adding and styling login page

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <div class="container">
 
       <form class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
+        <h2 class="form-signin-heading">Log in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
@@ -13,7 +13,7 @@
             <input type="checkbox" value="remember-me"> Remember me
           </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">LOG IN</button>
       </form>
 
     </div> <!-- /container -->             
