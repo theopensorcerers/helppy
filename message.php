33164f38a9dcdad38455735cf397058a26cab031
Mariela Barzallo <marielabarzallo@gmail.com>
2015-02-23 20:18:09 +0000

    add message page + style it

--- message.php
+++ message.php
@@ -1,25 +1,84 @@
 <?php include "includes/header.html" ?>
-<div class="container inbox">
 
-    <div class="row">
-      <div class="col-sm-8 requests">
-      	<h4>Can you help User 1?</h4>
-       <div class="row">
-            <div col-xs-8 col-sm-2>
-            DUE DATE
-            </div>
-            <div col-xs-8 col-sm-2>
-            REQUIRED SKILL
-            </div>
-             <div col-xs-8 col-sm-2>
-            YES
-            </div>
-             <div col-xs-8 col-sm-2>
-            NO
+
+<div class="container contacts">
+
+        <!-- Sidebar contacts/other users-->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Conversations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">User 1</a>
+                </li>
+                <li>
+                    <a href="#">User 2</a>
+                </li>
+                <li>
+                    <a href="#">User 3</a>
+                </li>
+                <li>
+                    <a href="#">User 4</a>
+                </li>
+                <li>
+                    <a href="#">User 5</a>
+                </li>
+                <li>
+                    <a href="#">User 6 </a>
+                </li>
+                
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row request">
+                    <div class="col-lg-12 reply_request">
+                        <h1>Can you help User 1?</h1>
+                        
+                        <a href="#menu-toggle" class="btn btn-default" id="reply_btn">Reply</a>
+                    </div>
+
+                    <div class="col-lg-12 reply_request">
+                        <div class="request_details">
+                            <h4>Due Date</h4> <p>01/01/01</p>
+                        </div> <!-- end of due date -->
+                        <div class="request_details">
+                            <h4>Skill</h4> <p>php skill</p>
+                        </div> <!-- end of skill -->
+                        <div class="request_details">
+                            <a href="#menu-toggle" class="btn btn-default" id="yesno_btn"><h4>yes</h4></a>
+                        </div> <!-- end of yes button-->
+                        <div class="request_details">
+                            <a href="#menu-toggle" class="btn btn-default" id="yesno_btn"><h4>no</h4></a>
+                        </div> <!-- end of no button-->
+                    </div>
+                    <hr></hr>
+                </div>
+                <div class="col-lg-12 message_history">
+                    <div class="message"><p>Hello could you help me with the frontend design of my website?</p></div>
+                    <div class="own_message"><p>Hi there, when do you need it for?</p></div>
+                    <div class="message"><p>I need it for next month</p></div>
+                    <div class="own_message"><p>That sounds like enought time to do it</p></div>
+
+
+                </div>
+                <div class="col-lg-12 write_message">
+                <textarea class="form-control" rows="6"></textarea>
+                <a href="#menu-toggle" class="btn btn-default" id="send_btn">Send message</a>
+                </div>
             </div>
         </div>
-      </div>
-      <div class="col-md-4 conversations">.col-md-1</div>
-      
+        <!-- /#page-content-wrapper -->
+
     </div>
-</div>
+    <!-- /#wrapper -->
+
+    
+
+</body>
