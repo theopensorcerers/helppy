eefd15fda074d15d5a9e91416719395703c1142c
Anais Moisy <anais.moisy@gmail.com>
2015-03-16 22:21:46 +0000

    style of drop down menu on all the web site (aware that I overwrite bootstrap to have the same style on every page)

---
+++ about_us.php
@@ -0,0 +1,23 @@
+<?php $page = 'home'; ?>
+<?php include "includes/header.html" ?>
+
+
+?>
+		<!-- Main jumbotron for a primary marketing message or call to action -->
+		<div class="jumbotron">
+			<div class="text-center">
+				<a class="logo"> 
+					<img src="images/logo.png" >
+				</a> 
+				<p>
+					<br>
+					A free platform to collaborate, offer and receive skills
+					<p> <strong> FIND </strong> the skills you are missing <br>
+						<strong> GET </strong> the help needed <br>
+						<strong> FOSTER </strong> collaboration <br>
+						<strong> CREATE </strong> great projects 
+					</p>
+				</p>
+			 	
+
+<?php include "includes/footer.html" ?>
