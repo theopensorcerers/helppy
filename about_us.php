529c78c20ce071725068fb7b311acfeeb49aaf62
Anais Moisy <anais.moisy@gmail.com>
2015-03-19 11:06:03 +0000

    About us page

--- about_us.php
+++ about_us.php
@@ -5,19 +5,65 @@
 ?>
 		<!-- Main jumbotron for a primary marketing message or call to action -->
 		<div class="jumbotron">
-			<div class="text-center">
-				<a class="logo"> 
-					<img src="images/logo.png" >
-				</a> 
-				<p>
+
+
+			<div class="container">
+				<div class="text-center">
+					<a class="logo"> 
+						<img src="images/logo.png" >
+					</a> 
+				</div>
+					
+				<div class="space70"></div>
+
+				<div class="row">
+				  <div class="col-xs-12 col-md-3">
+				  	<h1>About Us</h1>
+				  	<a href="/our_values.php">
+				  		<h2>Our Values</h2>
+				  	</a>
+				  	<h2>How it works</h2>
+				  	<h2>Safety</h2>
+				  	<h2>Privacy Policy</h2>
+				  	<h2>Help</h2> 
+
+				 
+
+
+				  </div>
+				  <div class="col-xs-12 col-md-1">
+				  </div>
+				  <div class="col-xs-12 col-md-8">
+				  <p> 
+					Lorem ipsum dolor sit amet, sapien nam donec in, enim tortor venenatis luctus pede, rhoncus ligula maecenas faucibus, 
+					molestie vel, libero morbi taciti amet sit. Curabitur id quisque, arcu arcu faucibus vestibulum, sodales nam pede quam egestas penatibus, 
+					velit diam. Lectus ultricies sed ornare volutpat, sed tincidunt mauris. Dolor vel ante quam quam, ad erat vivamus etiam bibendum, nulla nibh erat, 
+					fringilla congue vestibulum. Mauris feugiat, non pede odio leo sodales, erat sit dictumst. Quisque hac pretium, lorem amet wisi volutpat amet, 
+					sed magna odio id tristique nibh. Vestibulum wisi lacus rhoncus felis. Ut pede rhoncus diam rhoncus sagittis duis, dapibus mollis. 
+					Lectus lectus vitae nullam enim tellus, posuere ullamcorper adipiscing donec, fermentum suspendisse ullamcorper, ipsum amet justo neque mauris enim eu, 
+					est aliquet volutpat et vestibulum vivamus. Turpis quis sit non nulla tortor, in suspendisse accumsan, sed erat elementum semper sit nulla integer, 
+					justo nec sit malesuada. Sit sit feugiat nunc pede, aliquam in quis taciti. Tempus turpis, est mi a, sodales vitae eu venenatis fusce justo, eu luctus sed metus purus velit.
 					<br>
-					A free platform to collaborate, offer and receive skills
-					<p> <strong> FIND </strong> the skills you are missing <br>
-						<strong> GET </strong> the help needed <br>
-						<strong> FOSTER </strong> collaboration <br>
-						<strong> CREATE </strong> great projects 
+					<br>
+					Sit amet, duis rutrum, eu adipiscing phasellus nec est arcu varius. Enim sit nunc mauris blandit imperdiet pellentesque, vulputate placerat condimentum vivamus, 
+					natoque pellentesque amet fusce. Ac nulla porttitor vel, metus nulla, sit sed, nulla orci mollis aliquam turpis primis posuere. Augue nibh mollis quam vestibulum, 
+					elit eros, mauris blandit in lacinia risus in faucibus, curabitur facilisis quam ea nec pellentesque. Lorem vel cumque condimentum in sem donec, 
+					ut sed feugiat sem egestas neque placerat, eget bibendum blandit dignissim feugiat, curabitur odio lorem, libero nullam ligula odio tincidunt. 
+					Vestibulum faucibus euismod augue, tempor orci, mauris purus augue tincidunt, neque amet fusce. Dolor suspendisse a enim dui luctus.
+					<br>
+					<br>
+					Eget tristique eu molestie nibh enim, volutpat tristique vel duis suspendisse nunc lorem, nec sit tristique lectus aliquam dictum per, sed vestibulum ad. 
+					Tempor tempor habitasse luctus id. Sed morbi eu sit lobortis, duis risus euismod porta neque suspendisse, amet volutpat. Nulla consectetuer, 
+					hymenaeos vel tincidunt magna, quisque vestibulum, mauris non nisl amet elit venenatis dignissim, donec nulla. Hendrerit vitae, 
+					ut odio egestas nullam augue nonummy adipiscing, elit consequat, ultricies iaculis nibh vulputate mattis mattis. Integer sollicitudin sodales, 
+					montes magna hendrerit non a viverra, et diam sapien odio. Odio ligula, pellentesque nostra erat, vehicula sagittis congue duis magnis sapien, 
+					metus curae maecenas aliquam mollis suspendisse euismod, donec lacus mauris.
 					</p>
-				</p>
+				  </div>
+				</div>
+			</div>
+		</div>
+		
 			 	
 
 <?php include "includes/footer.html" ?>
